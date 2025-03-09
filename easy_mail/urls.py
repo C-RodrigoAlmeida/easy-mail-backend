e66b6dfa70a7d4934b7597ae1ba7e51ff7a12f32
C-RodrigoAlmeida <c.almeidarodrigo@gmail.com>
2025-03-09 18:22:45 -0300

    added slash after every route to keep django pattern

--- easy_mail/urls.py
+++ easy_mail/urls.py
@@ -27,9 +27,9 @@ from drf_spectacular.views import (
 api_include = lambda model_urls: path("api/", include(model_urls))
 
 urlpatterns = [
-    path("api/schema", SpectacularAPIView.as_view(), name="schema"),
-    path('api/docs/swagger', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
-    path('api/docs/redoc', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
+    path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('admin/', admin.site.urls),
     api_include("src.contacts.urls"),
     api_include("src.accounts.urls"),
--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -10,8 +10,8 @@ user_viewset = UserViewSet.as_view({
 })
 
 urlpatterns = [
-    path('login', LoginView.as_view(), name='login'),
-    path('logout', LogoutView.as_view(), name='logout'),
-    path('session', SessionView.as_view(), name='session'),
-    path('account', user_viewset, name='user')
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('session/', SessionView.as_view(), name='session'),
+    path('account/', user_viewset, name='user')
 ]
